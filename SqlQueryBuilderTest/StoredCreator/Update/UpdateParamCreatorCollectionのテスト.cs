d3cc8295e88b5727c368fff3e4685bf721a2db58
adaman3568 <adaman3568@gmail.com>
2021-02-26 08:59:36 +0900

    一旦Updateのテストを書いている途中

---
+++ SqlQueryBuilderTest/StoredCreator/Update/UpdateParamCreatorCollectionのテスト.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+using SqlQueryBuilderCommon.Extentions;
+using SqlQueryBuilderCommon.StoredCreator.Update;
+
+namespace SqlQueryBuilderTest.StoredCreator.Update
+{
+    [TestClass]
+    public class UpdateParamCreatorCollectionのテスト
+    {
+        public static DataTable MockDataTable()
+        {
+            var dt = new DataTable("Test");
+            var col1 = dt.Columns.Add("取込", typeof(bool));
+            var col2 = dt.Columns.Add("カラム名", typeof(string));
+            var col3 = dt.Columns.Add("型名", typeof(string));
+            var col4 = dt.Columns.Add("型許容サイズ", typeof(int));
+            var col5 = dt.Columns.Add("デフォルト値", typeof(object));
+
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", null });
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", "subaru" });
+            dt.Rows.Add(new object[] {false, "車両名", "varchar", "200", "subaru"});
+
+            return dt;
+        }
+
+        [TestMethod]
+        public void GetHeaderParamStrのテスト()
+        {
+            var dt = MockDataTable();
+            var c = new UpdateParamCreatorCollection();
+            c.AddRow(dt.Rows.ToEnumerable());
+            var answer = $@"@車両名 varchar(200)
+,@車両名 varchar(200) = 'subaru'";
+            c.GetHeaderParamStr().Is(answer);
+        }
+
+        [TestMethod]
+        public void GetUpdateParamStrのテスト()
+        {
+            var dt = MockDataTable();
+            var c = new UpdateParamCreatorCollection();
+            c.AddRow(dt.Rows.ToEnumerable());
+            var answer = $@"車両名 = @車両名
+,車両名 = @車両名
+,車両名 = 'subaru'";
+            c.GetHeaderParamStr().Is(answer);
+        }
+    }
+}
---
+++ SqlQueryBuilderTest/StoredCreator/Update/UpdateStoredCreatorのテスト.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SqlQueryBuilderTest.StoredCreator.Update
+{
+    [TestClass]
+    public class UpdateStoredCreatorのテスト
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
