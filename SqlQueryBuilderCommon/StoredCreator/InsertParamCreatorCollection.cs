cfd47e8e645ef4f75e949b7a778ae48d52c58eac
adaman3568 <adaman3568@gmail.com>
2021-02-25 14:28:07 +0900

    テストまで一旦完了

---
+++ SqlQueryBuilderCommon/StoredCreator/InsertParamCreatorCollection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using SqlQueryBuilderCommon.Static;
+using SqlQueryBuilderCommon.StoredCreator.ParamCreator;
+
+namespace SqlQueryBuilderCommon.StoredCreator
+{
+    public class InsertParamCreatorCollection
+    {
+        private IEnumerable<InsertParamCreator> Creators;
+
+        private string _separator = $@"{Environment.NewLine},";
+
+        public InsertParamCreatorCollection()
+        {
+            Creators = new List<InsertParamCreator>();
+        }
+
+        public string GetHeaderParamStr()
+        {
+            var param = Creators.Where(c => c.IsHeader()). Select(c => c.GetHeaderParam());
+            return string.Join(_separator, param);
+        }
+
+        public string GetColumnStr()
+        {
+            var param = Creators.Where(c => c.IsImport()).Select(c => c.ColumnName);
+            return string.Join(_separator, param);
+        }
+
+        public string GetValueParamStr()
+        {
+            var param = Creators.Where(c => c.IsImport()).Select(c => c.GetValueParam());
+            return string.Join(_separator, param);
+        }
+
+        public void AddRow(DataRow row)
+        {
+            Creators = Creators.Append(new InsertParamCreator(row));
+        }
+
+        public void AddRow(IEnumerable<DataRow> rows)
+        {
+            var creators = new List<InsertParamCreator>(Creators);
+            creators.AddRange(rows.Select(r => new InsertParamCreator(r)));
+            Creators = creators;
+        }
+    }
+}
---
+++ SqlQueryBuilderTest/InsertParamCreatorCollectionのテスト.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Sockets;
+using SqlQueryBuilderCommon.Extentions;
+using SqlQueryBuilderCommon.StoredCreator;
+
+namespace SqlQueryBuilderTest
+{
+    [TestClass]
+    public class InsertParamCreatorCollectionのテスト
+    {
+        [TestMethod]
+        public void GetColumnStrのテスト()
+        {
+            var dt = new DataTable("Test");
+            var col1 = dt.Columns.Add("取込", typeof(bool));
+            var col2 = dt.Columns.Add("カラム名", typeof(string));
+            var col3 = dt.Columns.Add("型名", typeof(string));
+            var col4 = dt.Columns.Add("型許容サイズ", typeof(int));
+            var col5 = dt.Columns.Add("デフォルト値", typeof(object));
+
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", null });
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", "subaru" });
+            dt.Rows.Add(new object[] { false, "車両名", "varchar", "200", "subaru" });
+
+            var collection = new InsertParamCreatorCollection();
+            foreach (DataRow row in dt.Rows)
+            {
+                collection.AddRow(row);
+            }
+
+            var answer = $@"車両名
+,車両名
+,車両名";
+            collection.GetColumnStr().Is(answer);
+
+        }
+
+        [TestMethod]
+        public void GetHeaderParamStrのテスト()
+        {
+            var dt = new DataTable("Test");
+            var col1 = dt.Columns.Add("取込", typeof(bool));
+            var col2 = dt.Columns.Add("カラム名", typeof(string));
+            var col3 = dt.Columns.Add("型名", typeof(string));
+            var col4 = dt.Columns.Add("型許容サイズ", typeof(int));
+            var col5 = dt.Columns.Add("デフォルト値", typeof(object));
+
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", null });
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", "subaru" });
+            dt.Rows.Add(new object[] { false, "車両名", "varchar", "200", "subaru" });
+
+            var collection = new InsertParamCreatorCollection();
+            foreach (DataRow row in dt.Rows)
+            {
+                collection.AddRow(row);
+            }
+
+            var answer = $@"@車両名 varchar(200)
+,@車両名 varchar(200) = 'subaru'";
+            collection.GetHeaderParamStr().Is(answer);
+
+        }
+
+        [TestMethod]
+        public void GetValueParamStrのテスト()
+        {
+            var dt = new DataTable("Test");
+            var col1 = dt.Columns.Add("取込", typeof(bool));
+            var col2 = dt.Columns.Add("カラム名", typeof(string));
+            var col3 = dt.Columns.Add("型名", typeof(string));
+            var col4 = dt.Columns.Add("型許容サイズ", typeof(int));
+            var col5 = dt.Columns.Add("デフォルト値", typeof(object));
+
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", null });
+            dt.Rows.Add(new object[] { true, "車両名", "varchar", "200", "subaru" });
+            dt.Rows.Add(new object[] { false, "車両名", "varchar", "200", "subaru" });
+
+            var collection = new InsertParamCreatorCollection();
+            collection.AddRow(dt.Rows.ToEnumerable());
+
+            var answer = $@"@車両名
+,@車両名
+,'subaru'";
+            collection.GetValueParamStr().Is(answer);
+
+        }
+    }
+}
