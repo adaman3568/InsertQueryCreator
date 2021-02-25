a695f7984af789f9a1a0f1236e9081372b199be4
adaman3568 <adaman3568@gmail.com>
2021-02-25 14:26:37 +0900

    DataRowCollectionに拡張メソッドを追加

---
+++ SqlQueryBuilderCommon/Extentions/DataRowCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace SqlQueryBuilderCommon.Extentions
+{
+    public static class DataRowCollectionExtensions
+    {
+        public static IEnumerable<System.Data.DataRow> ToEnumerable(this DataRowCollection collection)
+        {
+            var rowEnum = collection.GetEnumerator();
+            while (rowEnum.MoveNext())
+            {
+                yield return (DataRow) rowEnum.Current;
+            }
+        }
+    }
+}
